d3d58464aa3d04dbb49e77e0f6c36081e10f8456
etimamfon <etimamfon>
2023-04-25 21:58:06 -0700

    0x14_js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const dataz = process.argv[3];
+
+fs.writeFile(filePath, dataz, 'utf-8', (error) => {
+  if (error) console.error(error);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,3 @@
+Scripting API JavaScript
+
+How to manipulate JSON data How to use request and fetch API How to read and write a file using fs module
